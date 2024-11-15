aa673de76012c217f1359cb062d49175a1c4926a
On23ZN <zoe.neuberger.23@lehre.mosbach.dhbw.de>
2024-11-15 21:35:22 +0100

    Einbinden ToDoItem.vue

---
+++ vue-project/src/components/ToDoList.vue
@@ -0,0 +1,23 @@
+<script>
+// Import der ToDoItem-Komponente
+import ToDoItem from './ToDoItem.vue';
+
+export default {
+  // Name der Komponente
+  name: 'ToDoList',
+  // Registrieren der ToDoItem-Komponente
+  components: {
+    ToDoItem
+  },
+  // Definieren des Datenmodells der Komponente
+  data() {
+    return {
+      // Die neue Aufgabe, die der Benutzer eingeben kann
+      newToDo: '',
+      // Array, das alle Aufgaben speichert
+      toDos: []
+    };
+  },
+}
+</script>
+
