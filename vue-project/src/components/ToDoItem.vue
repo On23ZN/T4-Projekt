c1938691fc0b9342b25a99aa152cafcd6e267541
On23ZN <zoe.neuberger.23@lehre.mosbach.dhbw.de>
2024-11-15 22:05:57 +0100

    Methode switchComplete

---
+++ vue-project/src/components/ToDoItem.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  // Name der Komponente
+  name: 'ToDoItem',
+  // Props sind Eingaben für die Komponente
+  props: {
+    // Die aktuelle Aufgabe, die als Objekt übergeben wird
+    todo: Object
+  },
+  // Methoden, um die Aufgabe zu verwalten
+  methods: {
+    // Methode zum Umschalten des Erledigt-Status
+    switchComplete() {
+      // Ändert den Status der Aufgabe (true/false)
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
