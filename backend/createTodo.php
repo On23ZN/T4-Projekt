6b1acddcf3a7ba38998c3c3811d5700fe5f990ab
On23ZN <zoe.neuberger.23@lehre.mosbach.dhbw.de>
2024-11-16 19:55:59 +0100

    Datenbank verbinden

---
+++ backend/createTodo.php
@@ -0,0 +1,11 @@
+<?php
+// Verbindung zur Datenbank herstellen
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "todo_app";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
