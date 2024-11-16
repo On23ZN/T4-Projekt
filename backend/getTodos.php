be69b6c611805f8c38b71df941454de98393b502
On23ZN <zoe.neuberger.23@lehre.mosbach.dhbw.de>
2024-11-16 20:34:10 +0100

    Aufgabenabfrage und Ergebnisverarbeitung

--- backend/getTodos.php
+++ backend/getTodos.php
@@ -12,4 +12,15 @@ if ($conn->connect_error) {
     die("Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
 
+// Alle Aufgaben abrufen
+$sql = "SELECT * FROM todos";
+$result = $conn->query($sql);
+
+$todos = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $todos[] = $row;
+    }
+}
+
 ?>
