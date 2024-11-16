d3ba611829ca5a77999e5db6deeb7dc353a02981
On23ZN <zoe.neuberger.23@lehre.mosbach.dhbw.de>
2024-11-16 20:03:18 +0100

    Verbindung prüfen und Titel aus Anfrage holen

--- backend/createTodo.php
+++ backend/createTodo.php
@@ -8,4 +8,11 @@ $dbname = "todo_app";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+ // Verbindung überprüfen
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+
+// Eingabedaten aus der Anfrage holen
+$title = $_POST['title'] ?? '';
 ?>
