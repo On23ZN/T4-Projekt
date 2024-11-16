5c6c77f0a38b016a48c7336c817e40d81ce34b37
On23ZN <zoe.neuberger.23@lehre.mosbach.dhbw.de>
2024-11-16 20:47:22 +0100

    Datenbankverbindung herstellen

---
+++ backend/updateTodo.php
@@ -0,0 +1,15 @@
+<?php
+// Verbindung zur Datenbank herstellen
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "todo_app";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verbindung überprüfen
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+
+?>
