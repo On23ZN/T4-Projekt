cf0116a1fba73f7fdcc240d02dd57a6c28cd97c4
On23ZN <zoe.neuberger.23@lehre.mosbach.dhbw.de>
2024-11-28 22:44:36 +0100

    Implementieren getTodos Methode

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -25,32 +25,54 @@ export default {
       // Überprüft, ob die Eingabe nicht leer ist
       if (this.newToDo.trim() !== '') {
          // Verwende fetch, um eine POST-Anfrage an das Backend zu senden
-      fetch('http://localhost/T4-Projekt/backend/createTodo.php', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({ title: this.newTodo })
-      })
-      .then(response => response.json()) // Antwort in JSON umwandeln
-      .then(data => {
-        if (data.message) {
-        // Fügt eine neue Aufgabe zum Array hinzu
-        this.todos.push({ text: this.newToDo, completed: false });
-        // Leert das Eingabefeld
-        this.newToDo = '';
-      } else {
-          console.error(data.error); // Fehlerbehandlung
-        }
-      })
-      .catch(error => console.error('Fehler:', error));
-    }
+        fetch('http://localhost/T4-Projekt/backend/createTodo.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: new URLSearchParams({ title: this.newTodo })
+        })
+        .then(response => response.json()) // Antwort in JSON umwandeln
+        .then(data => {
+          if (data.message) {
+            // Fügt eine neue Aufgabe zum Array hinzu
+            this.todos.push({ text: this.newToDo, completed: false });
+            // Leert das Eingabefeld
+            this.newToDo = '';
+          } else {
+            console.error(data.error); // Fehlerbehandlung
+          }
+        })
+        .catch(error => console.error('Fehler:', error));
+      }
     },
+    //Methode zum abrufen der Aufgabenliste
+    async getTodos() {
+      try {
+        // Get-Anfrage an Backend senden, um die Aufgabe abzurufen
+        const response = await fetch('http://localhost/T4-Projekt/backend/getTodos.php');
+        //Überprüfen, war Antwort erfolgreich
+        if (!response.ok) {
+          throw new Error('Netzwerkantwort war nicht ok');
+        }
+        //Wandle die Antwort in JSON um
+        const data = await response.json();
+        //Aktualisieren der Aufgabenliste mit den empfangenen Daten
+        this.todos = data; 
+      } catch (error) {
+        // Fehlerbehandlung: Fehlermeldung in der Konsole
+        console.error('Fehler beim Abrufen der Todos:', error);
+      }
+    }  
     // Methode zum Löschen einer Aufgabe basierend auf dem Index
     deleteToDo(index) {
       // Entfernt die Aufgabe aus dem Array
       this.todos.splice(index, 1);
     }
+  }
+  mounted() {
+  // Aufgaben abrufen, wenn die Komponente geladen wird
+  this.getTodos();
   }
 };
 </script>
